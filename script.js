ecf0c27e72d56eea0d33385a8ea448f5b098cbee
Shivaya <Shivaya@DESKTOP-IKN54RP>
2020-12-20 20:06:08 +0100

    Add project files.

---
+++ script.js
@@ -0,0 +1,185 @@
+let top100Array = ['#love', 'instagood', '#beautiful', '#art', '#happy', '#picoftheday', '#tbt', '#followme', '#like4like', '#instagram', '#instadaily',
+'#mylife', '#fun', '#beauty', '#instalike', '#life', '#likeforlike', '#follow4follow', '#igers', '#instamood', '#amazing', '#motivation', '#followforfollow', '#lifestyle', '#l4l', '#f4f', '#일상',
+'#bestoftheday', '#artist', '#instapic', '#inspiration', '#likeforfollow', '#home', '#memes', '#happiness', '#daily', '#explore', '#instalove', '#insta', '#luxury', '#instacool', '#relax',
+'#likes', '#quotes', '#picture', '#blogger', '#mood', '#instaphoto', '#nice', '#like4likes', '#igdaily', '#lfl', '#instagramers', '#like4follow', '#top', '#business', '#entrepreneur', '#world',
+'#fresh', '#success', '#instamoment', '#pcgaming', '#comfort', '#likethis', '#btc', '#like4tags'];
+
+
+
+
+let cryptoArray=['#cryptocurrency', '#bitcoin', '#crypto', '#blockchain', '#btc', '#forex', '#ethereum', '#money', '#bitcoinmining', '#trading',
+'#investment', '#forextrader', '#cryptotrading', '#business', '#entrepreneur', '#bitcoins', '#investing', '#bitcoinnews', '#cryptocurrencies', '#binaryoptions',
+ '#invest', '#cryptonews', '#bitcointrading', '#forextrading', '#trader', '#bitcoincash', '#litecoin', '#eth', '#investor', '#bhfyp', '#forexsignals', '#success',
+  '#coinbase', '#bitcoinprice', '#forexlifestyle', '#binance', '#finance', '#xrp', '#stocks', '#wealth', '#usa', '#blockchaintechnology', '#ripple', '#mining',
+   '#stockmarket', '#motivation', '#bitcoinexchange', '#hodl', '#altcoin', '#trade', '#wallstreet', '#millionaire', '#cryptoworld', '#financialfreedom', '#cryptomining',
+    '#daytrader', '#makemoney', '#forexlife', '#altcoins', '#cryptocurrencynews', '#crypto', '#bitcoin', '#cryptocurrency', '#blockchain', '#btc', '#ethereum', '#money',
+     '#forex', '#trading', '#entrepreneur', '#bitcoinmining', '#litecoin', '#bitcoins', '#investing', '#cryptocurrencies', '#bitcoinnews', '#eth', '#trader', '#investor',
+      '#business', '#invest', '#success', '#investment', '#millionaire', '#ripple', '#coinbase', '#wealth', '#binaryoptions', '#stocks', '#ico', '#business', '#entrepreneur',
+       '#motivation', '#success', '#marketing', '#love', '#money', '#mindset', '#inspiration', '#instagood', '#instagram', '#lifestyle', '#goals', '#entrepreneurship', '#life',
+        '#businessowner', '#smallbusiness', '#bhfyp', '#startup', '#motivational', '#follow', '#digitalmarketing', '#believe', '#design', '#fashion', '#branding',
+         '#work', '#like', '#bhfyp', '#fitness', '#hustle', '#inspire', '#positivevibes', '#realestate', '#finance', '#happy', '#socialmedia', '#businesswoman', '#entrepreneurlife',
+          '#happiness', '#motivational', '#covid', '#leadership', '#positivity', '#inspirational', '#art', '#photography', '#technology', '#socialmediamarketing',
+           '#photooftheday', '#selflove', '#marketingdigital', '#investing', '#wealth', '#bitcoin', '#businessman', '#success', '#quoteoftheday', '#forex', '#love'];
+
+
+let gamingArray=['#csgoinvestment', '#csgo', '#csgolounge', '#csgomemes', '#csgoskins', '#csgoskin', '#investment', '#profit', '#money', '#steam', '#steammarket',
+                '#marketcrash', '#csgocase', '#csgoknife', '#marketing', '#payitforward', '#trading', '#millionairelifestyle', '#millionaire', '#investor',
+                '#daytrader', '#daytrading', '#tradingmemes', '#trading', '#memes', '#funny', '#comedy', '#dank', '#dankmemes', '#hilarious', '#hills', '#fifamemes',
+                '#fifatrading', '#dankmemes', '#dankmeme', '#dailymemes', '#memesquad', '#finance', '#investment', '#investing', '#relateable', '#relateablememes',
+                '#hilariousmemes', '#fifa21', '#fifa', '#fifatrading', '#fifaultimateteam', '#ultimateteam', '#fifacoins', '#fifapoints', '#fut', '#weekendleague',
+                '#futchampions', '#squadbuilder', '#transfernews', '#neymar', '#neymarjr', '#futleaks', '#fifa21leaks', '#teamoftheweek', '#packopening', '#otw',
+                '#transferrumours', '#toty', '#fifasquad', '#teamoftheseason', '#futdraft', '#fifapacks', '#fifamobile', '#tots', '#fifa20', '#tiktok', '#minecraft'];
+
+
+let codingArray = ['#techmemes', '#meme', '#memes', '#memesdaily', '#memesfordays', '#memepage', '#memesfunny', '#funny', '#funnymemes', '#programmingmemes', '#programmingmeme',
+                    '#programmer', '#programmers', '#programmerlife', '#programminglife', '#programmingjokes', '#programminglanguage', '#programminghumor', '#programminghumour',
+                    '#coding', '#coder', '#coders', '#codinglife', '#codingjokes', '#codingmeme', '#codingmemes', '#codinghumor', '#coderslife', '#coderlife', '#coderlifestyle',
+                    '#python', '#pythonmemes', '#pythonprogramming', '#java', '#javascript', '#javascripts', '#html', '#csharp', '#instameme', '#instamemes', '#computer',
+                    '#computers', '#computermemes', '#internet', '#internetmemes', '#entertainment', '#humor', '#humour', '#gaming', '#gamingpc', '#gamingmemes'];
+
+
+
+function displayCrypto(){
+    //create an index of a random number 
+    //convert it into between 0 to length of cryptoArray[]
+
+    let index = Math.floor(Math.random()* cryptoArray.length);
+    let index2 = Math.floor(Math.random()* cryptoArray.length);
+    let index3 = Math.floor(Math.random()* cryptoArray.length);
+    let index4 = Math.floor(Math.random()* cryptoArray.length);
+    let index5 = Math.floor(Math.random()* cryptoArray.length);
+    let index6 = Math.floor(Math.random()* cryptoArray.length);
+    let index7 = Math.floor(Math.random()* cryptoArray.length);
+    let index8 = Math.floor(Math.random()* cryptoArray.length);
+    let index9 = Math.floor(Math.random()* cryptoArray.length);
+    let index10 = Math.floor(Math.random()* cryptoArray.length);
+    let index11 = Math.floor(Math.random()* cryptoArray.length);
+    let index12 = Math.floor(Math.random()* cryptoArray.length);
+    let index13 = Math.floor(Math.random()* cryptoArray.length);
+    let index14 = Math.floor(Math.random()* cryptoArray.length);
+    let index15 = Math.floor(Math.random()* cryptoArray.length);
+    let index16 = Math.floor(Math.random()* top100Array.length);
+    let index17 = Math.floor(Math.random()* top100Array.length);
+    let index18 = Math.floor(Math.random()* top100Array.length);
+    let index19 = Math.floor(Math.random()* top100Array.length);
+    let index20 = Math.floor(Math.random()* top100Array.length);
+    let index21 = Math.floor(Math.random()* top100Array.length);
+    let index22 = Math.floor(Math.random()* top100Array.length);
+    let index23 = Math.floor(Math.random()* top100Array.length);
+    let index24 = Math.floor(Math.random()* top100Array.length);
+    let index25 = Math.floor(Math.random()* top100Array.length);
+
+
+   
+    //display the quote of that index
+    let div=document.querySelector('#crypto');
+    let quote=`<div class="card">
+    <img src="icons/bitcoin-logo.png">
+    <p>${cryptoArray[index]} ${cryptoArray[index2]} ${cryptoArray[index3]} ${cryptoArray[index4]} ${cryptoArray[index5]} ${cryptoArray[index6]} ${cryptoArray[index7]} ${cryptoArray[index8]} ${cryptoArray[index9]} ${cryptoArray[index10]} ${cryptoArray[index11]}
+    ${cryptoArray[index12]} ${cryptoArray[index13]} ${cryptoArray[index14]} ${cryptoArray[index15]} ${top100Array[index16]} ${top100Array[index17]} ${top100Array[index18]} ${top100Array[index19]} ${top100Array[index20]} ${top100Array[index21]} ${top100Array[index22]}
+     ${top100Array[index23]} ${top100Array[index24]} ${top100Array[index25]}</p>
+    <img src="icons/bitcoin-logo.png">
+   </div>
+
+    `;
+    div.innerHTML=quote;
+    
+}
+
+
+
+function displayGaming(){
+    //create an index of a random number 
+    //convert it into between 0 to length of cryptoArray[]
+
+    let index = Math.floor(Math.random()* gamingArray.length);
+    let index2 = Math.floor(Math.random()* gamingArray.length);
+    let index3 = Math.floor(Math.random()* gamingArray.length);
+    let index4 = Math.floor(Math.random()* gamingArray.length);
+    let index5 = Math.floor(Math.random()* gamingArray.length);
+    let index6 = Math.floor(Math.random()* gamingArray.length);
+    let index7 = Math.floor(Math.random()* gamingArray.length);
+    let index8 = Math.floor(Math.random()* gamingArray.length);
+    let index9 = Math.floor(Math.random()* gamingArray.length);
+    let index10 = Math.floor(Math.random()* gamingArray.length);
+    let index11 = Math.floor(Math.random()* gamingArray.length);
+    let index12 = Math.floor(Math.random()* gamingArray.length);
+    let index13 = Math.floor(Math.random()* gamingArray.length);
+    let index14 = Math.floor(Math.random()* gamingArray.length);
+    let index15 = Math.floor(Math.random()* gamingArray.length);
+    let index16 = Math.floor(Math.random()* gamingArray.length);
+    let index17 = Math.floor(Math.random()* gamingArray.length);
+    let index18 = Math.floor(Math.random()* gamingArray.length);
+    let index19 = Math.floor(Math.random()* gamingArray.length);
+    let index20 = Math.floor(Math.random()* top100Array.length);
+    let index21 = Math.floor(Math.random()* top100Array.length);
+    let index22 = Math.floor(Math.random()* top100Array.length);
+    let index23 = Math.floor(Math.random()* top100Array.length);
+    let index24 = Math.floor(Math.random()* top100Array.length);
+    let index25 = Math.floor(Math.random()* top100Array.length);
+
+
+   
+    //display the quote of that index
+    let div=document.querySelector('#gaming');
+    let quote=`<div class="card2">
+    <img src="icons/console.png">
+    <p>${gamingArray[index]} ${gamingArray[index2]} ${gamingArray[index3]} ${gamingArray[index4]} ${gamingArray[index5]} ${gamingArray[index6]} ${gamingArray[index7]} ${gamingArray[index8]} ${gamingArray[index9]} ${gamingArray[index10]} ${gamingArray[index11]}
+    ${gamingArray[index12]} ${gamingArray[index13]} ${gamingArray[index14]} ${gamingArray[index15]} ${gamingArray[index16]} ${gamingArray[index17]} ${gamingArray[index18]} ${gamingArray[index19]} ${gamingArray[index20]} ${top100Array[index21]} ${top100Array[index22]}
+     ${top100Array[index23]} ${top100Array[index24]} ${top100Array[index25]}</p>
+    <img src="icons/console.png">
+   </div>
+
+    `;
+    div.innerHTML=quote;
+    
+}
+
+
+function displayCoding(){
+    //create an index of a random number 
+    //convert it into between 0 to length of cryptoArray[]
+
+    let index = Math.floor(Math.random()* codingArray.length);
+    let index2 = Math.floor(Math.random()* codingArray.length);
+    let index3 = Math.floor(Math.random()* codingArray.length);
+    let index4 = Math.floor(Math.random()* codingArray.length);
+    let index5 = Math.floor(Math.random()* codingArray.length);
+    let index6 = Math.floor(Math.random()* codingArray.length);
+    let index7 = Math.floor(Math.random()* codingArray.length);
+    let index8 = Math.floor(Math.random()* codingArray.length);
+    let index9 = Math.floor(Math.random()* codingArray.length);
+    let index10 = Math.floor(Math.random()* codingArray.length);
+    let index11 = Math.floor(Math.random()* codingArray.length);
+    let index12 = Math.floor(Math.random()* codingArray.length);
+    let index13 = Math.floor(Math.random()* codingArray.length);
+    let index14 = Math.floor(Math.random()* codingArray.length);
+    let index15 = Math.floor(Math.random()* codingArray.length);
+    let index16 = Math.floor(Math.random()* codingArray.length);
+    let index17 = Math.floor(Math.random()* codingArray.length);
+    let index18 = Math.floor(Math.random()* top100Array.length);
+    let index19 = Math.floor(Math.random()* top100Array.length);
+    let index20 = Math.floor(Math.random()* top100Array.length);
+    let index21 = Math.floor(Math.random()* top100Array.length);
+    let index22 = Math.floor(Math.random()* top100Array.length);
+    let index23 = Math.floor(Math.random()* top100Array.length);
+    let index24 = Math.floor(Math.random()* top100Array.length);
+    let index25 = Math.floor(Math.random()* top100Array.length);
+
+
+   
+    //display the quote of that index
+    let div=document.querySelector('#coding');
+    let quote=`<div class="card3">
+    <img src="icons/web-programming.png">
+    <p>${codingArray[index]} ${codingArray[index2]} ${codingArray[index3]} ${codingArray[index4]} ${codingArray[index5]} ${codingArray[index6]} ${codingArray[index7]} ${codingArray[index8]} ${codingArray[index9]} ${codingArray[index10]} ${codingArray[index11]}
+    ${codingArray[index12]} ${codingArray[index13]} ${codingArray[index14]} ${codingArray[index15]} ${codingArray[index16]} ${codingArray[index17]} ${top100Array[index18]} ${top100Array[index19]} ${top100Array[index20]} ${top100Array[index21]} ${top100Array[index22]}
+     ${top100Array[index23]} ${top100Array[index24]} ${top100Array[index25]}</p>
+    <img src="icons/web-programming.png">
+   </div>
+
+    `;
+    div.innerHTML=quote;
+    
+}
+
